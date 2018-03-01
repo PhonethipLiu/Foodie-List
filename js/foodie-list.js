cafab3f883a79e8705ed13005807f6d4f3ad38ba
Phonethip <phonethip.liu@gmail.com>
2018-02-28 22:06:25 -0600

    added sass and some index html

--- js/foodie-list.js
+++ js/foodie-list.js
@@ -7,32 +7,33 @@
 // > Contents of `restaurants.json` file
 // > Contents of `cities.json` file
 
-console.log("foodie-list.js working");
+// console.log("foodie-list.js working");
 
 
 
-let dataRequest = new XMLHttpRequest();
-dataRequest.addEventListener("load", dataRequestComplete);
-dataRequest.addEventListener("error", dataRequestFailed);
+// let dataRequest = new XMLHttpRequest();
+// dataRequest.addEventListener("load", dataRequestComplete);
+// dataRequest.addEventListener("error", dataRequestFailed);
 
-function dataRequestComplete(event) {
-    console.log("loading restaurants json");
-        let restaurantsData = JSON.parse(event.target.responseText);
-        console.log("restaurants Data", restaurantsData);
-        showData(restaurantsData);
-}
+// function dataRequestComplete(event) {
+//     console.log("loading restaurants json");
+//         let restaurantsData = JSON.parse(event.target.responseText);
+//         console.log("restaurants Data", restaurantsData);
+//         showData(restaurantsData);
+// }
 
-//MVP: 1) When the user first visits the page, all restaurants should be listed in order of their rating(ratings are from 1 to 10). The restaurants are displayed from highest rated to lowest rated.
-function showData(cities){
-    let restaurantsDiv = document.getElementById("cities");
-    let restaurantsData = "";
+// //MVP: 1) When the user first visits the page, all restaurants should be listed in order of their rating(ratings are from 1 to 10). The restaurants are displayed from highest rated to lowest rated.
 
-    for(let i = 0; i < restaurants.length; i++){
-        let restaurantsItem = restaurants[item];
-        restaurantsData += `<h3>${restaurantsItem.restaurant} + " " + ${restaurantsItem.my_rating}</h3>`;
+// function showData(cities){
+//     let restaurantsDiv = document.getElementById("cities");
+//     let restaurantsData = "";
 
-    }
-}
+//     for(let i = 0; i < restaurants.length; i++){
+//         let restaurantsItem = restaurants[item];
+//         restaurantsData += `<h3>${restaurantsItem.restaurant} + " " + ${restaurantsItem.my_rating}</h3>`;
+
+//     }
+// }
 
 
 
