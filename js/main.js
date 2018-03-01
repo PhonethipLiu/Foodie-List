482843b33eddd905448e94aed924e77495dc91c8
Phonethip <phonethip.liu@gmail.com>
2018-03-01 09:32:58 -0600

    set css and begin code

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 "use strict";
 
+console.log("main js is working so far");
+
+let restaurantData = require('./foodie-list');
+
+let btn = document.getElementById("dropdownMenuButton");
+btn.addEventListener("click",restaurantData.getAllRestaurants);
